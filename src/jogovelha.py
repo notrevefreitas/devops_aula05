34aebbc1087d8357671654b1b6f722054c9a529b
notrevefreitas <36283397+notrevefreitas@users.noreply.github.com>
2018-03-09 21:53:09 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,13 +1,15 @@
-definicializar():
-tab= [ ]
-for i in range(3):
-linha = [ ]
-for j in range(3):
-linha.append(".")
-tab.append(linha)
-returntab
-defmain( ):
-jogo = inicializar( )
-print(jogo)
-if__name__ == "__main__":
-main()
+def inicializar():
+	tab = [ ]
+	for i in range(3):
+		linha = [ ]
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
+
+def main( ):
+	jogo = inicializar( )
+	print (jogo)
+
+if __name__ == "__main__":
+	main()
